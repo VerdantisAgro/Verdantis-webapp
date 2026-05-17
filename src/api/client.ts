ed3db7705834a600d1f042ed76af6caca541c5ed
caiolucasxz55 <caiolucasxz55@gmail.com>
2026-05-17 14:38:14 -0300

    Fix: update API baseURL to use production endpoint

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: "https://vitsapi.onrender.com/",
   headers: {
     "Content-Type": "application/json",
   },
